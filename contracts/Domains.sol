047ba40ff68fa6e62fbf327b72d8cd9945f2f095
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-02-20 07:42:44 -0500

    Added record setting funcs to contract

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,17 +7,33 @@ import "hardhat/console.sol";
 contract Domains {
     //mapping from domain name to address
     mapping(string => address) public domains;
+    //mapping that stores the record for domain
+    mapping(string => string) public records;
 
     constructor() {
         console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
     }
 
     function register(string calldata name) public {
+        require(domains[name] == address(0));
         domains[name] = msg.sender;
-        console.log("%s has registered for %s domain", msg.sender);
+        console.log("%s has registered for a domain", msg.sender);
     }
 
     function getAddress(string calldata name) public view returns (address) {
         return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,21 @@
 const main = async () => {
+
+  const [owner, randomGuy] = await hre.ethers.getSigners(); 
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
-  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by: ", owner.address);
+  console.log("Contract deployed to: ", domainContract.address);
+
+  const txn = await domainContract.register("doom");
+	await txn.wait();
+
+  const getowner = await domainContract.getAddress("doom");
+  console.log("Owner of domain:", getowner);
+
+  const txn2 = await domainContract.connect(randomGuy).setRecord("doom", "got this domain!");
+  await txn2.wait();
+
 };
 
 const runMain = async () => {
